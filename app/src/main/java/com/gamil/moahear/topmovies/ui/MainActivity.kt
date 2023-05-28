a4b8e54ff5a13497037e22b95364592c400dc17e
Mostafa Ahmadi <moahear@gmail.com>
2023-05-28 08:59:39 +0330

    show or hide bottom navigation view, design fragment_splash.xml, add TextViewLightStyle

--- app/src/main/java/com/gamil/moahear/topmovies/ui/MainActivity.kt
+++ app/src/main/java/com/gamil/moahear/topmovies/ui/MainActivity.kt
@@ -2,11 +2,45 @@ package com.gamil.moahear.topmovies.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.gamil.moahear.topmovies.R
+import com.gamil.moahear.topmovies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Binding
+    private lateinit var binding:ActivityMainBinding
+    //NavController
+    private val navController by lazy{
+        binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+
+            bottomNavigationView.setupWithNavController(navController)
+            //show or hide bottom navigation on fragment
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                if (destination.id==R.id.splashFragment){
+                    bottomNavigationView.visibility = View.GONE
+                }
+                else{
+                    bottomNavigationView.visibility = View.VISIBLE
+                }
+            }
+
+        }
+
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp()|| super.onNavigateUp()
     }
 }
--- app/src/main/java/com/gamil/moahear/topmovies/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/gamil/moahear/topmovies/ui/splash/SplashFragment.kt
@@ -6,16 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.gamil.moahear.topmovies.R
+import com.gamil.moahear.topmovies.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
+    private lateinit var binding:FragmentSplashBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding=FragmentSplashBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+
+        }
+    }
 }
