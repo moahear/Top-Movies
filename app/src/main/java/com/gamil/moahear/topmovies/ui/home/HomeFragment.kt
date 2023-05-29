f37bb1feb06708853fe739cf8aabf43d13df0661
Mostafa Ahmadi <moahear@gmail.com>
2023-05-29 17:14:13 +0330

    design fragment_home.xml and item_home_top_movie.xml

--- app/src/main/java/com/gamil/moahear/topmovies/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gamil/moahear/topmovies/ui/home/HomeFragment.kt
@@ -6,17 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.gamil.moahear.topmovies.R
+import com.gamil.moahear.topmovies.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-
-
+    private lateinit var binding:FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        binding= FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+
+        }
     }
 
 }
