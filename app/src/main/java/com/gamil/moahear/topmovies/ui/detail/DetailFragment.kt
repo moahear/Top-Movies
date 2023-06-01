de3db00be3b6cefaa7f4b0491d4056c2cdd90b32
Mostafa Ahmadi <moahear@gmail.com>
2023-06-01 14:55:38 +0330

    Design fragment_detail

--- app/src/main/java/com/gamil/moahear/topmovies/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/gamil/moahear/topmovies/ui/detail/DetailFragment.kt
@@ -5,18 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.gamil.moahear.topmovies.R
+import com.gamil.moahear.topmovies.databinding.FragmentDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class DetailFragment : Fragment() {
-
-
+    private lateinit var binding:FragmentDetailBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        binding=FragmentDetailBinding.inflate(inflater,container,false)
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+
+        }
     }
 
 }
